a3fc0e9f93214b162bbff842e20ef86b727bf426
Sitti Rahma <99602629+sirahyh@users.noreply.github.com>
2023-01-27 18:33:34 +0800

    responsive navbar

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -1,8 +1,18 @@
-const bar = document.getElementById('bar');
+const bar = document.querySelector('bar');
 const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+bar.addEventListener('click', () => {
+    console.log("tombol bar tertekan")
+})
+
+close.addEventListener('click', () => {
+    console.log("tombol bar tertekan")
+})
 
 if (bar){
     bar.addEventListener('click', () => {
        nav.classList.add('active'); 
+       console.log("tombol bar tertekan")
     })
 }
